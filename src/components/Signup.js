4019eb2b9d487af7b4a34171c17fd3dba097ff51
gaurav asawa <gaurav.asawa@asychron.com>
2022-06-02 23:30:47 +0530

    commit

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -10,90 +10,115 @@ const Signup = (props) => {
     password: "",
   });
 
-    const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({});
 
   const handelSubmit = async (e) => {
     e.preventDefault();
-    if(!handelValidations()){
-    const response = await fetch(`http://localhost:8080/api/auth/createuser`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+    
+    if (handelValidations()) {
+      console.log(credentials.username);
+      const response = await fetch(
+        `http://localhost:8080/api/auth/createuser`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
 
-      body: JSON.stringify({
-        name: credentials.username,
-        email: credentials.email,
-        password: credentials.password,
-      }),
-    });
-    const json = await response.json();
-    if (json.success) {
-      //redirect and save the auth token
-      localStorage.setItem("token", json.authToken);
-      navigation("/");
-      props.showAlert("Welcome! Account Created.", "success");
-    }else{}
-    }  
-     else {
+          body: JSON.stringify({
+            name: credentials.username,
+            email: credentials.email,
+            password: credentials.password,
+          }),
+        }
+      );
+      const json = await response.json();
+      if (json.success) {
+        //redirect and save the auth token
+        localStorage.setItem("token", json.authToken);
+        navigation("/");
+        props.showAlert("Welcome! Account Created.", "success");
+      } else {
+      }
+    } else {
       props.showAlert("Invalid!", "danger");
     }
-
   };
 
   const onChange = (e) => {
     setCredentials({ ...credentials, [e.target.name]: e.target.value });
   };
 
+  const handelValidations = () => {
+    let UserName = credentials.username;
+    let Email = credentials.email;
+    let password = credentials.password;
+    let error = {};
+    let formisValid = true;
 
-   const handelValidations = () => {
-     let Email = credentials.email;
-     let password = credentials.password;
-     let error = {};
-     let formisValid = true;
+    //===================Validation Event Email==============================
+    if (!UserName.toString().match(/^.+\s.+$/g)) {
+      if (!UserName) {
+        formisValid = false;
+        error["UserName"] = "Cannot be empty";
+      } else if (
+        typeof UserName !== "undefined" 
+      ) {
+        if (
+          !UserName.toString().match(/^[A-Za-z]{3,20}/g)
+        ) {
+          formisValid = false;
+          error["UserName"] = "Please Enter a valid UserName"
+          +"Min 3 charachter to 20 character max ";
+        }
+      }
+    }
 
-     //===================Validation Event Name==============================
-     if (!Email.toString().match(/^.+\s.+$/g)) {
-       if (!Email) {
-         formisValid = false;
-         error["Email"] = "Cannot be empty";
-       } else if (
-         typeof Email !== "undefined" &&
-         !Email.toString().match(/^.+\s.+$/g)
-       ) {
-         if (
-           !Email.toString().match(/^.+\s.+$/g) &&
-           !Email.toString().match(
-             /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/
-           )
-         ) {
-           formisValid = false;
-           error["Email"] = "Please Enter a valid Email";
-         }
-       }
-     }
+    //===================Validation Event Email==============================
+    if (!Email.toString().match(/^.+\s.+$/g)) {
+      if (!Email) {
+        formisValid = false;
+        error["Email"] = "Cannot be empty";
+      } else if (
+        typeof Email !== "undefined" &&
+        !Email.toString().match(/^.+\s.+$/g)
+      ) {
+        if (
+          !Email.toString().match(/^.+\s.+$/g) &&
+          !Email.toString().match(
+            /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/
+          )
+        ) {
+          formisValid = false;
+          error["Email"] = "Please Enter a valid Email";
+        }
+      }
+    }
 
-     ///====================Password Validation ====================================
-     if (!password.toString().match(/^.+\s.+$/g)) {
-       if (!password) {
-         formisValid = false;
-         error["password"] = "Cannot be empty";
-       } else if (
-         typeof password !== "undefined" &&
-         !password.toString().match(/^.+\s.+$/g)
-       ) {
-         if (!password.toString().match(/^.+\s.+$/g)) {
-           formisValid = false;
-           error["password"] = "Please Enter a valid password";
-         }
-       }
-     }
+    ///====================Password Validation ====================================
+    if (!password.toString().match(/^.+\s.+$/g)) {
+      if (!password) {
+        formisValid = false;
+        error["password"] = "Cannot be empty";
+      } else if (
+        typeof password !== "undefined" &&
+        !password.toString().match(/^.+\s.+$/g)
+      ) {
+        if (
+          !password.toString().match(/^[0-9A-Za-z@#]{6,12}/g) &&
+          !password.toString().match(/^.+\s.+$/g)
+        ) {
+          formisValid = false;
+          error["password"] = "Please Enter a valid password";
+        }
+      }
+    }
 
-     ///=======End of Validations
+    ///=======End of Validations======================
 
-     setErrors(error);
-     return formisValid;
-   };
+    setErrors(error);
+    return formisValid;
+  };
 
   return (
     <div className="container" style={{ marginTop: "110px" }}>
